32d177e06e3b4a6b48a62b4fc3c1c219171f3c76
Toren Arginteanu <targinteanu@outlook.com>
2023-09-03 16:23:02 -0400

    rodent SSEP data test in progress

---
+++ RodentSSEPDataFilt.m
@@ -0,0 +1,43 @@
+%% load file
+TDTPATH = 'TDTMatlabSDK';
+addpath(genpath(TDTPATH));
+data = TDTbin2mat('AC5-230830-130841/AC5-230830-130841');
+
+%% define parameters of stim 
+% monophasic, fixed
+%  ^         __________
+%  |        |          |
+% AmpA      |          |
+%  |        |          |
+%  v   _____|          |_____
+%            <--DurA-->
+PeriodA = 0.8;  % ms - between pulses?
+CountA  = 1;    % pulse count? 
+AmpA    = 1500; % uA
+DurA    = 0.2;  % ms
+DelayA  = 0;    % ms
+ChanA   = 1; 
+
+%% organize data from file
+dta = data.snips.SSEP.data; 
+t_stim = data.snips.SSEP.ts;
+fs = data.snips.SSEP.fs;
+chan = data.snips.SSEP.chan; uchan = unique(chan);
+
+dta_t_chan = cell(2, length(uchan));
+for idx = 1:length(uchan)
+    ch = uchan(idx);
+    chIdx = chan == ch;
+    dta_t_chan(1, idx) =  dta(chIdx,:);
+    dta_t_chan(2, idx) = t_stim(chIdx);
+end
+
+dta = zeros([size(dta_t_chan(1,1)), length(uchan)]);
+t_stim = zeros([length(dta_t_chan(2,1)), length(uchan)]);
+for idx = 1:length(uchan)
+    dta(:,:,idx)  = dta_t_chan(1, idx);
+    t_stim(:,idx) = dta_t_chan(2, idx);
+end
+
+t_trl = (1:size(dta, 2)/fs) - .3; % ~ -.3 to +1 s
+g_trl = t_trl; % noise reference, uA
